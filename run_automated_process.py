78754ce5e35d0839a7f4f884ff21f53f05417750
Jaime Galiana <74818454+jaime-galiana@users.noreply.github.com>
2024-06-04 15:44:33 +0100

    Add files via upload

---
+++ run_automated_process.py
@@ -0,0 +1,98 @@
+"""
+Code part of FYP: "Aerodynamic shape optimization of wings with compliant winglets"
+
+Jaime Galiana Herrera
+Imperial College London
+Feb 2024
+
+Code designed for the automatic generation of wing geometry utilizing FLEXOP aircraft geometry and OpenVSP.
+It allows for exploring different cant and sweep angle configurations. The total wing span is fixed, 
+thus, the different winglet configurations affect the main wing span and this is taken into account in the code.
+
+"""
+
+import os
+import subprocess
+import argparse
+
+def create_directory(path):
+    """Creates a directory if it doesn't exist."""
+    try:
+        os.makedirs(path, exist_ok=True)
+    except OSError as e:
+        print(f"Error creating directory: {path} - {e}")
+
+
+def modify_script(filename, np, mem, time, cant, sweep):
+    """Modifies a script by replacing specific lines with new values."""
+    try:
+        with open(filename, "r") as file:
+            replaced_content = ""
+            for line in file:
+                stripped_line = line.strip()
+                if "#PBS -l walltime=" in stripped_line:
+                    new_line = f"#PBS -l walltime={time}:00:00"
+                elif "#PBS -l select=1:ncpus=" in stripped_line:
+                    new_line = f"#PBS -l select=1:ncpus={np}:mem={mem}gb"
+                elif "geometry_generation.py -c" in stripped_line:
+                    new_line = f"python3 /rds/general/user/jg2219/ephemeral/Final-try/bin/geometry_generation.py -c {cant} -s {sweep}"
+                elif "mesh_generation.py -np" in stripped_line:
+                    new_line = f"python3 /rds/general/user/jg2219/ephemeral/Final-try/bin/mesh_generation.py -np {np}"
+                else:
+                    new_line = line
+                replaced_content += new_line + "\n"
+            file.close()
+        with open(filename, "w") as write_file:
+            write_file.write(replaced_content)
+            write_file.close()
+    except FileNotFoundError as e:
+        print(f"Error: File not found - {filename} - {e}")
+    except Exception as e:
+        print(f"Error modifying script: {filename} - {e}")
+
+def main(np, mem, time):
+    list_cant = [-120, -105, -90, -75, -60, -45, -30, -15, 0, 15, 30, 45, 60, 75, 90, 105, 120]
+    list_sweep = [-30, -25, -15, -5, 5, 15, 25, 30, 35, 40]
+
+    main_folder = "/rds/general/user/jg2219/ephemeral/Final-try"
+    template_folder = "bin/submit_template.pbs"
+
+    for cant in list_cant:
+        for sweep in list_sweep:
+            # Change directory to main
+            os.chdir(main_folder)
+
+            # Change to output directory
+            output_folder = f"output/winglet_c{cant}_s{sweep}"
+            create_directory(output_folder)
+
+            # Copy template to output folder
+            subprocess.run(['cp', os.path.join(main_folder, template_folder), os.path.join(main_folder, output_folder, "submit.pbs")])
+
+            # Copy winggen.vspscript file
+            subprocess.run(['cp', os.path.join(main_folder, "bin/winggen.vspscript"), os.path.join(main_folder, output_folder, "winggen.vspscript")])
+
+            # Copy macro file
+            subprocess.run(['cp', os.path.join(main_folder, "bin/macro.java"), os.path.join(main_folder, output_folder, "macro.java")])
+
+            # Change directory to output/wing_c{}_s{}
+            os.chdir(output_folder)
+            modify_script("submit.pbs", np, mem, time, cant, sweep)
+
+            try:
+                subprocess.run(["qsub", "submit.pbs"], check=True)
+            except subprocess.CalledProcessError as e:
+                print(f"Error executing qsub: {e}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Script to submit batch jobs')
+    parser.add_argument('-np', type=int, help='Number parallel processes')
+    parser.add_argument('-mem', type=int, help='Memory of each process')
+    parser.add_argument('-time', type=int, help='Job time')
+
+    args = parser.parse_args()
+
+    if args.np is None or args.mem is None or args.time is None:
+        parser.error("Please provide both -np, -mem and -time arguments.")
+
+    main(args.np, args.mem, args.time)
